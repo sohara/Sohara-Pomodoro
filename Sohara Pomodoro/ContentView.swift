734a8d8e94b9357132e0dc8a39d7a6350fe71204
Sean O'Hara <sohara@sohara.com>
2023-11-12 17:59:46 -0500

    Add basic timer functionality and display

--- Sohara Pomodoro/ContentView.swift
+++ Sohara Pomodoro/ContentView.swift
@@ -8,16 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var timeRemaining = 1500 // 25 minutes in seconds
+    @State private var timer: Timer?
+
+    func startTimer() {
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+            if timeRemaining > 0 {
+                timeRemaining -= 1
+            } else {
+                timer?.invalidate()
+            }
+        }
+    }
+    
+    func timeString(from totalSeconds: Int) -> String {
+          let minutes = totalSeconds / 60
+          let seconds = totalSeconds % 60
+          return String(format: "%02d:%02d", minutes, seconds)
+      }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(timeString(from: timeRemaining))
+                .font(.largeTitle)
+                .monospacedDigit()
+            Button("Start") {
+                startTimer()
+            }
+            Button("Pause") {
+                timer?.invalidate()
+            }
+            Button("Reset") {
+                timer?.invalidate()
+                timeRemaining = 1500
+            }
         }
         .padding()
     }
 }
+
+
+
 
 #Preview {
     ContentView()
