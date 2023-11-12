7909b73e89ea07b48060c7d4874c680bc3b22472
Sean O'Hara <sohara@sohara.com>
2023-11-12 17:37:02 -0500

    Initial Commit

---
+++ Sohara Pomodoro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sohara Pomodoro
+//
+//  Created by Sean O'Hara on 2023-11-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sohara Pomodoro/Sohara_PomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sohara_PomodoroApp.swift
+//  Sohara Pomodoro
+//
+//  Created by Sean O'Hara on 2023-11-12.
+//
+
+import SwiftUI
+
+@main
+struct Sohara_PomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
