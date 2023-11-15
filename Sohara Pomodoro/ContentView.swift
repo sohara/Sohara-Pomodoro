6c3acd0febb02181f991b996e3bd6bf7ded1eec4
Sean O'Hara <sohara@sohara.com>
2023-11-15 16:00:08 -0500

    Add alert and sound when time's up - Also extract pomodoroDuration constant for reuse

--- Sohara Pomodoro/ContentView.swift
+++ Sohara Pomodoro/ContentView.swift
@@ -6,13 +6,22 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct ContentView: View {
-    @State private var timeRemaining = 1500 // 25 minutes in seconds
+    let pomodoroDuration = 1500 // 25 minutes in seconds
+    
+    @State private var timeRemaining: Int
     @State private var timer: Timer?
     @State private var isTimerRunning = false
     @State private var timerStarted = false
+    @State private var showAlert = false
+    @State private var audioPlayer: AVAudioPlayer?
 
+    init() {
+        _timeRemaining = State(initialValue: pomodoroDuration)
+       }
+    
     func startTimer() {
         // Only create a timer if it's not already running
         if !isTimerRunning {
@@ -20,12 +29,18 @@ struct ContentView: View {
                 if timeRemaining > 0 {
                     timeRemaining -= 1
                 } else {
-                    stopTimer()
+                    timeExpired()
                 }
             }
             isTimerRunning = true
             timerStarted = true
         }
+    }
+    
+    func timeExpired() {
+        stopTimer()
+        showAlert = true
+        playSound()
     }
     
     func stopTimer() {
@@ -42,11 +57,27 @@ struct ContentView: View {
         }
     }
     
+    func resetTimer() {
+        stopTimer()
+        timeRemaining = pomodoroDuration // Reset duration
+        timerStarted = false
+    }
+    
     func timeString(from totalSeconds: Int) -> String {
-          let minutes = totalSeconds / 60
-          let seconds = totalSeconds % 60
-          return String(format: "%02d:%02d", minutes, seconds)
-      }
+        let minutes = totalSeconds / 60
+        let seconds = totalSeconds % 60
+        return String(format: "%02d:%02d", minutes, seconds)
+    }
+    
+    func playSound() {
+        guard let url = Bundle.main.url(forResource: "reception-bell-14620", withExtension: "mp3") else { return }
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url)
+            audioPlayer?.play()
+        } catch {
+            print("Couldn't load the sound file")
+        }
+    }
     
     var body: some View {
         VStack {
@@ -63,11 +94,20 @@ struct ContentView: View {
             .opacity(timerStarted ? 1 : 0.3)
             Button("Reset") {
                 stopTimer()
-                timeRemaining = 1500
+                timeRemaining = pomodoroDuration
                 timerStarted = false
             }
         }
         .padding()
+        .alert(isPresented: $showAlert) {
+            Alert(
+                title: Text("Time's Up!"),
+                message: Text("The Pomodoro timer has finished."),
+                dismissButton: .default(Text("OK")) {
+                            self.resetTimer()
+                }
+            )
+        }
     }
 }
 
